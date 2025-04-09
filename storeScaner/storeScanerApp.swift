d309b1d77e31cfd37894a4a9d3243d33ab1ddead
duszekjk <jacek@duszekjk.com>
2025-04-09 11:57:28 +0200

    Initial Commit

---
+++ storeScaner/storeScanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeScanerApp.swift
+//  storeScaner
+//
+//  Created by Jacek Kałużny on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct storeScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
